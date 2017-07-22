0143c098c4fd2b4743718bcb98c12ae69857113d
Andy <andysofranac@gmail.com>
2017-07-22 16:25:27 +0000

    Plan your day script added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-var items = ["The Met", "Central Park", "The Guggenheim", "Carl Schurz Park", "Roosevelt Island Tramway"]
+var arrExplore = ["The Met", "The Guggenheim","Central Park","Carl Schurz Park","Roosevelt Island Tramway"];
+var arrEat = ["Nick's Pizza", "Toloache","Bar Coastal","EJ's Luncheonette","Uva"];
+var arrDrink = ["The Stumble Inn", "Ryan's Daughter", "Treadwell Park","The Penrose","Doc Watson's"];
 
-for (var 1= 0, i < 100000; i++)
+var pickItem = function (arr){
+  var chooseItem = arr [Math.floor(Math.random() * 5)]; 
+  return chooseItem
+    
+};
+
+$('#buttonSelect').click(function(){
+  var value = pickItem(arrExplore);
+  $('#itemExplore').val(value)
+   value = pickItem(arrEat);
+  $('#itemEat').val(value)
+  value = pickItem(arrDrink);
+  $('#itemDrink').val(value)
+});
+
+// $('#buttonSelect').click(function(){
+//   var value = pickItem(arrEat);
+//   $('#itemEat').val(value)
+// });
+
+// $('#buttonSelect').click(function(){
+//   var value = pickItem(arrDrink);
+//   $('#itemDrink').val(value)
+// });
