0eb8ff0859f8c5d621ffb8ba6a60e85bdcb58cc4
Andy <andysofranac@gmail.com>
2017-07-25 23:14:55 +0000

    Fixed Plan My Day function across the 3 pages

--- js/script.js
+++ js/script.js
@@ -1,28 +1,49 @@
-var arrExplore = ["The Met", "The Guggenheim","Central Park","Carl Schurz Park","Roosevelt Island Tramway"];
-var arrEat = ["Nick's Pizza", "Toloache","Bar Coastal","EJ's Luncheonette","Uva"];
-var arrDrink = ["The Stumble Inn", "Ryan's Daughter", "Treadwell Park","The Penrose","Doc Watson's"];
-
 var pickItem = function (arr){
   var chooseItem = arr [Math.floor(Math.random() * 5)]; 
   return chooseItem
-    
 };
 
-$('#buttonSelect').click(function(){
-  var value = pickItem(arrExplore);
+
+// UES Day Planner
+var arrUesExplore = ["The Met", "The Guggenheim","Central Park","Carl Schurz Park","Roosevelt Island Tramway"];
+var arrUesEat = ["Nick's Pizza", "Toloache","Bar Coastal","EJ's Luncheonette","Uva"];
+var arrUesDrink = ["The Stumble Inn", "Ryan's Daughter", "Treadwell Park","The Penrose","Doc Watson's"];
+
+$('#buttonSelectUES').click(function(){
+  var value = pickItem(arrUesExplore);
   $('#itemExplore').val(value)
-   value = pickItem(arrEat);
+   value = pickItem(arrUesEat);
   $('#itemEat').val(value)
-  value = pickItem(arrDrink);
+  value = pickItem(arrUesDrink);
   $('#itemDrink').val(value)
 });
 
-// $('#buttonSelect').click(function(){
-//   var value = pickItem(arrEat);
-//   $('#itemEat').val(value)
-// });
+// Village Day Planner
+var arrVillageExplore = ["Washington Square Park", "NYU","Comedy Cellar","IFC Center","Blue Note"];
+var arrVillageEat = ["Joe's Pizza", "Cuba","Cotenna","Miss Lily's","Tortaria"];
+var arrVillageDrink = ["Down the Hatch", "Carroll Place", "Fat Cat","Amity Hall","The Garret"];
 
-// $('#buttonSelect').click(function(){
-//   var value = pickItem(arrDrink);
-//   $('#itemDrink').val(value)
-// });
+$('#buttonSelectVillage').click(function(){
+  var value = pickItem(arrVillageExplore);
+  $('#itemExplore').val(value)
+  value = pickItem(arrVillageEat);
+  $('#itemEat').val(value)
+  value = pickItem(arrVillageDrink);
+  $('#itemDrink').val(value)
+});
+
+
+// Williamsburg Day Planner
+var arrWilliamsburgExplore = ["Brooklyn Bowl", "Smorgasburg / Brookyln Flea","Music Hall of Williamsburg","Bushwick Inlet Park","The Sketchbook Project"];
+var arrWilliamsburgEat = ["Sweet Chick", "Miss Favela's","Fette Sau","Peter Luger's","Vinnie's Pizzeria"];
+var arrWilliamsburgDrink = ["Brooklyn Brewery", "Berry Park", "The Whiskey Brooklyn","Radegast Hall & Biergarten","Freehold"];
+
+$('#buttonSelectWilliamsburg').click(function(){
+  var value = pickItem(arrWilliamsburgExplore);
+  $('#itemExplore').val(value)
+  value = pickItem(arrWilliamsburgEat);
+  $('#itemEat').val(value)
+  value = pickItem(arrWilliamsburgDrink);
+  $('#itemDrink').val(value)
+});
+
